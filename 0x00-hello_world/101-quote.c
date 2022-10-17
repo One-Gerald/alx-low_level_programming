9fd712552373fcf0c49f187185c329ff9ce985d2
One-Gerald <onegerald@gmail.com>
2022-10-17 06:09:39 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
+
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (0);
+
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+
+	return (1);
 
 }
